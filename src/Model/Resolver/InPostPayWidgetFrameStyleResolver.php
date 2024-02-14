0160f52c92eca7df251872eec5631bbd75a7b54e
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2024-02-14 11:17:05 +0100

    INPAY-121 - Added GraphQL store config providers for widget layout and display settings.

---
+++ src/Model/Resolver/InPostPayWidgetFrameStyleResolver.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InPost\InPostPayGraphQl\Model\Resolver;
+
+use InPost\InPostPay\Provider\Config\LayoutConfigProvider;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Store\Api\Data\StoreInterface;
+
+class InPostPayWidgetFrameStyleResolver implements ResolverInterface
+{
+    private const CLASSIC_FRAME_STYLE = 'classic';
+
+    /**
+     * @param LayoutConfigProvider $layoutConfigProvider
+     */
+    public function __construct(
+        private readonly LayoutConfigProvider $layoutConfigProvider
+    ) {
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): string
+    {
+        $websiteId = null;
+        $store = $context->getExtensionAttributes()->getStore();
+        if ($store instanceof StoreInterface) {
+            $websiteId = (int)$store->getWebsiteId();
+        }
+        $frameStyle = $this->layoutConfigProvider->getFrameStyle($websiteId);
+
+        return !empty($frameStyle) ? $frameStyle : self::CLASSIC_FRAME_STYLE;
+    }
+}

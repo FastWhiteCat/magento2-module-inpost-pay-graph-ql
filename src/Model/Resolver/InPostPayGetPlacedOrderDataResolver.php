c74f13107727877d01a77a489bd4d6095ea82c81
Piotr Kozioł <piotr.koziol@fastwhitecat.com>
2024-11-07 12:39:53 +0100

    INPAY-267 - Added controller for getting basket binding api key + InPost Pay API request

--- src/Model/Resolver/InPostPayGetPlacedOrderDataResolver.php
+++ src/Model/Resolver/InPostPayGetPlacedOrderDataResolver.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace InPost\InPostPayGraphQl\Model\Resolver;
 
-use InPost\InPostPay\Api\Data\InPostPayOrderInterface;
 use InPost\InPostPay\Api\Data\InPostPayQuoteInterface;
 use InPost\InPostPay\Api\InPostPayOrderRepositoryInterface;
 use InPost\InPostPay\Exception\BasketNotFoundException;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
-use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
-use InPost\InPostPay\Model\ResourceModel\InPostPayQuote as InPostPayQuoteResource;
+use Magento\Quote\Model\Quote;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Checkout\Model\Session as CheckoutSession;
@@ -25,22 +22,18 @@ use Psr\Log\LoggerInterface;
  * @SuppressWarnings(PHPMD.CookieAndSessionMisuse)
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class InPostPayGetPlacedOrderDataResolver extends InPostBasketResolver implements ResolverInterface
+class InPostPayGetPlacedOrderDataResolver implements ResolverInterface
 {
-    private const ORDER_ID = 'order_id';
-    private const STATUS = 'status';
-    private const STATUS_LABEL = 'status_label';
-    private const CART_VERSION = 'cart_version';
+    public const SUCCESS_RESULT_KEY = 'success';
+    public const ERROR_RESULT_KEY = 'error';
+    public const REDIRECT_RESULT_KEY = 'redirect';
 
     public function __construct(
-        GetCartForUser $cartForUser,
-        LoggerInterface $logger,
+        private readonly LoggerInterface $logger,
         private readonly InPostPayOrderRepositoryInterface $inPostPayOrderRepository,
-        private readonly InPostPayQuoteResource $inPostPayQuoteResource,
         private readonly OrderRepositoryInterface $orderRepository,
         private readonly CheckoutSession $checkoutSession
     ) {
-        parent::__construct($cartForUser, $logger);
     }
 
     /**
@@ -48,76 +41,28 @@ class InPostPayGetPlacedOrderDataResolver extends InPostBasketResolver implement
      */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): array
     {
-        $basketId = $this->extractBasketId($args ?? []);
-        $cartVersion = '';
+        $basketBindingApiKey = $this->extractBasketBindingApiKey($args ?? []);
 
         try {
-            $inPostPayData = $this->inPostPayQuoteResource->getCartVersionAndOrderId($basketId);
+            $inPostPayOrder = $this->inPostPayOrderRepository->getByBasketBindingApiKey($basketBindingApiKey);
+            $order = $this->orderRepository->get($inPostPayOrder->getOrderId());
+            $this->setLastOrder($order);
 
-            if (empty($inPostPayData)) {
-                $inPostPayData = $this->getInPostPayOrderDataByBasketId($basketId);
-            }
-
-            if (empty($inPostPayData)) {
-                throw new BasketNotFoundException(__('Could not find a basket with ID:%1', $basketId));
-            }
-
-            $cartVersion = (string)($inPostPayData[InPostPayQuoteInterface::CART_VERSION] ?? '');
-            $orderId = (int)($inPostPayData[InPostPayOrderInterface::ORDER_ID] ?? 0);
-
-            if ($orderId) {
-                $order = $this->orderRepository->get($orderId);
-                $this->setLastOrder($order);
-                $result = $this->preparePlacedOrderResponse($order, $cartVersion);
-            } else {
-                $result = $this->prepareUnplacedOrderResponse($cartVersion);
-            }
-
-            return $result;
-        } catch (BasketNotFoundException $e) {
-            $this->logger->error($e->getMessage(), ['basket_id' => $basketId]);
-
-            $errorResponse = $this->prepareErrorResponse(
-                InPostBasketResolver::ACTION_REJECT,
-                __($e->getMessage())->render()
+            return [
+                self::SUCCESS_RESULT_KEY => true,
+                self::REDIRECT_RESULT_KEY => '' //TODO:: get from config
+            ];
+        } catch (BasketNotFoundException | LocalizedException $e) {
+            $this->logger->error(
+                $e->getMessage(),
+                [InPostPayQuoteInterface::BASKET_BINDING_API_KEY => $basketBindingApiKey]
             );
 
-            $errorResponse[self::CART_VERSION] = $cartVersion;
-
-            return $errorResponse;
-        } catch (LocalizedException $e) {
-            $this->logger->error($e->getMessage(), ['basket_id' => $basketId]);
-
-            $errorResponse = $this->prepareErrorResponse(
-                InPostBasketResolver::ACTION_REJECT,
-                __('There was an error while checking if order was placed in InPost Pay Mobile App.')->render()
-            );
-
-            $errorResponse[self::CART_VERSION] = $cartVersion;
-
-            return $errorResponse;
+            return [
+                self::SUCCESS_RESULT_KEY => false,
+                self::ERROR_RESULT_KEY => $e->getMessage()
+            ];
         }
-    }
-
-    private function preparePlacedOrderResponse(OrderInterface $order, string $cartVersion): array
-    {
-        // @phpstan-ignore-next-line
-        $statusLabel = (string)$order->getStatusLabel();
-        return [
-            self::CART_VERSION => $cartVersion,
-            self::ACTION => self::ACTION_REDIRECT,
-            self::ORDER_ID => (string)$order->getIncrementId(),
-            self::STATUS => (string)$order->getStatus(),
-            self::STATUS_LABEL => $statusLabel
-        ];
-    }
-
-    private function prepareUnplacedOrderResponse(string $cartVersion): array
-    {
-        return [
-            self::CART_VERSION => $cartVersion,
-            self::ACTION => self::ACTION_REFRESH
-        ];
     }
 
     private function setLastOrder(OrderInterface $order): void
@@ -129,22 +74,10 @@ class InPostPayGetPlacedOrderDataResolver extends InPostBasketResolver implement
         $this->checkoutSession->setLastOrderStatus($order->getStatus());
     }
 
-    private function extractBasketId(array $data): string
+    private function extractBasketBindingApiKey(array $data): string
     {
-        $basketId = $data['basket_id'] ?? '';
+        $basketBindingApiKey = $data[InPostPayQuoteInterface::BASKET_BINDING_API_KEY] ?? '';
 
-        return is_scalar($basketId) ? (string)$basketId : '';
-    }
-
-    private function getInPostPayOrderDataByBasketId(string $basketId): array
-    {
-        try {
-            $inPostPayOrder = $this->inPostPayOrderRepository->getByBasketId($basketId);
-            $inPostPayData[InPostPayOrderInterface::ORDER_ID] = $inPostPayOrder->getOrderId();
-        } catch (NoSuchEntityException | LocalizedException $e) {
-            $inPostPayData = [];
-        }
-
-        return $inPostPayData;
+        return is_scalar($basketBindingApiKey) ? (string)$basketBindingApiKey : '';
     }
 }
--- src/Model/Resolver/InPostPayInitBasketResolver.php
+++ src/Model/Resolver/InPostPayInitBasketResolver.php
@@ -4,24 +4,27 @@ declare(strict_types=1);
 
 namespace InPost\InPostPayGraphQl\Model\Resolver;
 
-use InPost\InPostPay\Service\PayDataProcessor;
+use InPost\InPostPay\Api\Data\InPostPayQuoteInterface;
+use InPost\InPostPay\Service\InitBasketProcessor;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Quote\Model\Quote;
 use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
-use Magento\Framework\App\Action\Context as AppActionContext;
 use Psr\Log\LoggerInterface;
 
-class InPostPayInitBasketResolver extends InPostBasketResolver implements ResolverInterface
+class InPostPayInitBasketResolver implements ResolverInterface
 {
+    public const SUCCESS_RESULT_KEY = 'success';
+    public const ERROR_RESULT_KEY = 'error';
+
     public function __construct(
-        GetCartForUser $cartForUser,
-        LoggerInterface $logger,
-        private readonly AppActionContext $appActionContext,
-        private readonly PayDataProcessor $payDataProcessor
+        private readonly GetCartForUser $cartForUser,
+        private readonly LoggerInterface $logger,
+        private readonly InitBasketProcessor $initBasketProcessor
     ) {
-        parent::__construct($cartForUser, $logger);
     }
 
     /**
@@ -35,13 +38,41 @@ class InPostPayInitBasketResolver extends InPostBasketResolver implements Resolv
         try {
             $quote = $this->getQuoteFromCartMaskIdAndContext($cartMaskId, $context);
             $quoteId = (is_scalar($quote->getId())) ? (int)$quote->getId() : 0;
+            $inPostPayQuote = $this->initBasketProcessor->process($quoteId);
 
-            // @phpstan-ignore-next-line
-            return $this->payDataProcessor->process($quoteId, $inputData, $this->appActionContext->getRequest());
+            return [
+                self::SUCCESS_RESULT_KEY => true,
+                InPostPayQuoteInterface::BASKET_BINDING_API_KEY => $inPostPayQuote->getBasketBindingApiKey()
+            ];
         } catch (LocalizedException $e) {
             $this->logger->error($e->getMessage(), ['cart_mask_id' => $cartMaskId]);
 
-            return $this->prepareErrorResponse(self::ACTION_REJECT, $e->getMessage());
+            return [
+                self::SUCCESS_RESULT_KEY => false,
+                self::ERROR_RESULT_KEY => $e->getMessage()
+            ];
         }
+    }
+
+    private function getQuoteFromCartMaskIdAndContext(string $maskedCartId, ContextInterface $context): Quote
+    {
+        // @phpstan-ignore-next-line
+        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        // @phpstan-ignore-next-line
+        $userId = (int)$context->getUserId();
+        $quote = $this->cartForUser->execute($maskedCartId, $userId, $storeId);
+
+        if (!$quote->getIsActive()) {
+            throw new LocalizedException(__('Quote is inactive.'));
+        }
+
+        return $quote;
+    }
+
+    private function extractCartMaskId(array $data): string
+    {
+        $maskedCartId = $data['cart_id'] ?? '';
+
+        return is_scalar($maskedCartId) ? (string)$maskedCartId : '';
     }
 }
